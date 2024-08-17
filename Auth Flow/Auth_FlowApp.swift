fa94fb53fd6d40269cc808b0fd4468107a1576b6
JeisunDev <56722713+Jason-Karcheski@users.noreply.github.com>
2024-08-17 13:58:22 +0100

    Initial Commit

---
+++ Auth Flow/Auth_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Auth_FlowApp.swift
+//  Auth Flow
+//
+//  Created by Jason Karcheski on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Auth_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
